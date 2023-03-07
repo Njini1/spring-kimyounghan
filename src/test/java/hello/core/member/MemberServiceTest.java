f649a8ae0c9a7fed02eed3dc65521af8ec528434
Njini1 <dbwls6627@gmail.com>
2023-03-07 19:10:31 +0900

    springContainerAndSpringBean

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,30 @@
+package hello.core.member;
+
+import hello.core.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService;
+
+    @BeforeEach
+    void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
+    @Test
+    void join() {
+        //given
+        Member member = new Member(1L, "test1", Grade.VIP); //1L의 L은 Long의미. L쓰지 않을 경우 오류
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
